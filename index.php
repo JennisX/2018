c2c065747af5855289e4f7ac3c8c2dcc95cbb793
Jennis <Jennis@qq.com>
2018-10-04 14:25:20 +0800

    2018.10.04 1.2

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
     </h1>
     <script>
         document.getElementsByTagName("h1")[0].style = "color:blue";
+        document.getElementsByTagName("h1")[0].innerText = "Minecraft";
     </script>
 </body>
 </html>
